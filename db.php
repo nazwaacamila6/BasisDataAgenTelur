fb4d8c8716a11c7e91e7c8ae20c2bf7cafe4edbf
nazwaacamila6 <meelahtrash@gmail.com>
2024-12-19 12:38:40 +0700

    Add files via upload

---
+++ db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=10.80.3.7:3306;dbname=db_Agentelur',
+    'username' => 'nazwa',
+    'password' => 'camila',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
